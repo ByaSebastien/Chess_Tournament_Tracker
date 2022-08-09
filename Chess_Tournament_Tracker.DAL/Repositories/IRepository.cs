51bff1e0fab50ff3180f7a361171d5c1d14a68ff
Matthias <a@a>
2022-08-09 08:54:13 +0200

    repository base

---
+++ Chess_Tournament_Tracker.DAL/Repositories/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess_Tournament_Tracker.DAL.Repositories
+{
+    internal interface IRepository
+    {
+    }
+}
