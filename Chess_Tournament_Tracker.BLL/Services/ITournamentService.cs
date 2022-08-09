d319c22f25d56548bd76ed547f11207d9f1e881c
Matthias <a@a>
2022-08-09 16:23:40 +0200

    Tournament service in progress

---
+++ Chess_Tournament_Tracker.BLL/Services/ITournamentService.cs
@@ -0,0 +1,22 @@
+﻿using Chess_Tournament_Tracker.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess_Tournament_Tracker.BLL.Services
+{
+    public interface ITournamentService
+    {
+        Tournament Add();
+        bool Delete(Guid id);
+        bool Update();
+        Tournament GetById (Guid id);
+        IEnumerable<Tournament> GetAll();
+        void AddPlayer(Guid UserId);
+        void DeletePlayer(Guid UserId);
+        
+
+    }
+}
