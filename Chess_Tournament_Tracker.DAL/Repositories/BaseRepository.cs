efb47584ac2ffefe8b55b05be431e782ba39a138
Matthias <a@a>
2022-08-09 08:58:13 +0200

    modif

--- Chess_Tournament_Tracker.DAL/Repositories/BaseRepository.cs
+++ Chess_Tournament_Tracker.DAL/Repositories/BaseRepository.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 namespace Chess_Tournament_Tracker.DAL.Repositories
 {
     public class RepositoryBase<TEntity> : IRepository<TEntity>
+        where TEntity : class
     {
     }
 }
